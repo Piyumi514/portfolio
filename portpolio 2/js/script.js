6d4fbe0146a9865fa452526c1eeb306d9babf4a3
Piyumi514 <164770115+Piyumi514@users.noreply.github.com>
2024-10-05 22:03:16 +0530

    Add files via upload

---
+++ portpolio 2/js/script.js
@@ -0,0 +1,7 @@
+/* ======================typing animation======================== */
+var typed = new Typed(".typing",{
+    strings:["Undergraduate of Sabaragamuwa University Sri Lanka","Web Designer","Mobile app developer","UX/UI Designer","Graphic Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
